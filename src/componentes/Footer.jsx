18ea2ac39dc5b27bd0fdf2e3b4ed6d09c000d394
Raul Juarez <rauljuarez.fernando@gmail.com>
2026-06-03 23:19:36 -0300

    cambio en footer y like

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer className="footer">
-      <p>© 2026 - Mi Portfolio Ruly++</p>
+      <p>© 2026 Mejor Calle | React • GitHub • Vercel</p>
 
       <div className="social">
         <a href="#">🌐</a>
--- src/componentes/Like/Like.jsx
+++ src/componentes/Like/Like.jsx
@@ -2,14 +2,25 @@ import { useState } from "react";
 import "./Like.css";
 
 function Like() {
+  const [liked, setLiked] = useState(false);
   const [likes, setLikes] = useState(0);
+
+  const handleLike = () => {
+    if (liked) {
+      setLikes(likes - 1);
+    } else {
+      setLikes(likes + 1);
+    }
+
+    setLiked(!liked);
+  };
 
   return (
     <button
-      className="like-btn"
-      onClick={() => setLikes(likes + 1)}
+      className={`like-btn ${liked ? "active" : ""}`}
+      onClick={handleLike}
     >
-      ❤️ <span>{likes}</span>
+      {liked ? "❤️" : "🤍"} {likes}
     </button>
   );
 }
