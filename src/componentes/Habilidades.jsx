317a3bd9a389b5b44939fbb7677b3d3e572f62d8
Raul Juarez <rauljuarez.fernando@gmail.com>
2026-06-04 13:48:00 -0300

    modifico texto en Habilidades

--- src/componentes/Habilidades.jsx
+++ src/componentes/Habilidades.jsx
@@ -1,15 +1,18 @@
 import "./Habilidades.css";
 
 function Habilidades() {
-  const skills = ["HTML", "CSS", "JavaScript", "React", "Git", "GitHub", "Vercel"];
+  const skills = ["HTML", "CSS", "JavaScript", "React", "Git", "GitHub", "Vercel", "IA"];
     
   return (
     <section className="habilidades">
       <h3>🛠️ Tecnologías utilizadas</h3>
 
+
       <p className="skills-descripcion">
     Este sitio fue desarrollado utilizando las siguientes tecnologías:
   </p>
+  🤖 Además de las tecnologías utilizadas, este proyecto contó con el apoyo de herramientas de inteligencia artificial como asistente de aprendizaje y desarrollo, permitiendo explorar soluciones, mejorar el diseño y adquirir nuevas habilidades durante el proceso.
+
     
     <div className="skills-container">
       {skills.map((skill, index) => (
