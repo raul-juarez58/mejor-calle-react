812918f7d9a3007b7971f7c2998ae689e11ab314
Raul Juarez <rauljuarez.fernando@gmail.com>
2026-06-13 23:35:16 -0300

    Likes persistentes con Supabase

--- src/componentes/Like/Like.jsx
+++ src/componentes/Like/Like.jsx
@@ -1,26 +1,53 @@
-import { useState } from "react";
+import React, { useState,useEffect } from "react";
+import { supabase } from "../../supabase/client";
 import "./Like.css";
 
 function Like() {
-  const [liked, setLiked] = useState(false);
   const [likes, setLikes] = useState(0);
 
-  const handleLike = () => {
-    if (liked) {
-      setLikes(likes - 1);
-    } else {
-      setLikes(likes + 1);
-    }
+  const cargarLikes = async () => {
+    const { data,error } = await supabase
+      .from("likes")
+      .select("*")
+      .eq("id", 1)
+      .limit(1);
 
-    setLiked(!liked);
+      console.log ("DATA", data);
+      console.log ("ERROR", error);
+
+      if (!error && data.length > 0) {
+        setLikes(data[0].contador);
+      }
+  };
+
+  useEffect(() => {
+    cargarLikes();
+  }, []);
+
+  const handleLike = async () => {
+     const nuevoValor = likes + 1;
+
+     console.log("LIKES ACTUAL:", likes);
+     console.log("NUEVO VALOR:", nuevoValor);
+
+     const { data, error } = await supabase
+         .from("likes")
+         .update({ contador: nuevoValor })
+         .eq("id", 1);
+         
+      console.log("UPDATE DATA:", data);     
+      console.log ("UPDATE ERROR:", error);   
+
+      setLikes(nuevoValor);
+
   };
 
   return (
     <button
-      className={`like-btn ${liked ? "active" : ""}`}
+      className="like-btn"
       onClick={handleLike}
     >
-      {liked ? "❤️" : "🤍"} {likes}
+      ❤️ {likes}
     </button>
   );
 }
